68bf630553099f552eb53098e1e66ad2edcbf865
aleafreinn <alifazib72@gmail.com>
2023-09-12 05:11:34 +0800

    patch: fix pathname for images

--- src/assets/profilePic.js
+++ src/assets/profilePic.js
@@ -1,9 +1,9 @@
-import Profile1 from "./Profile1.png";
-import Profile2 from "./Profile2.png";
-import Profile3 from "./Profile3.png";
-import Profile4 from "./Profile4.png";
-import Profile5 from "./Profile5.png";
-import Profile6 from "./Profile6.png";
+import Profile1 from "./profile1.png";
+import Profile2 from "./profile2.png";
+import Profile3 from "./profile3.png";
+import Profile4 from "./profile4.png";
+import Profile5 from "./profile5.png";
+import Profile6 from "./profile6.png";
 
 const preImageList = [
   Profile1,
